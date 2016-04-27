6f4ad734283c32a927ca42193e191b46c4bf41ac
jkarmaz6 <jkarmaz6@gmail.com>
2016-04-27 16:21:49 -0400

    Create Counties.js

---
+++ lab6/KarmazinLab6/Counties.js
@@ -0,0 +1,3 @@
+var counties = {"type":"FeatureCollection","features":[
+{"type":"Feature","id":"Adams", "properties":{"name":"Adams County","Population":10156,"Bernie Votes":4066, "Hillary":3846},"geometry":{"type":"Polygon","coordinates":
+  [[[
